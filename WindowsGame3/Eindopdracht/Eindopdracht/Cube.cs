a18dc7a431dad450b720076d8633543d9bcbeb34
Hidde Jansen <hiddejansen@me.com>
2012-05-29 18:09:06 +0200

    Eindopdracht - begin

---
+++ WindowsGame3/Eindopdracht/Eindopdracht/Cube.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Eindopdracht
+{
+	public class Cube : DrawableGameComponent
+	{
+		private VertexPositionColor[] points;
+
+		public VertexPositionColor[] Points
+		{
+			get { return points; }
+			set { points = value; }
+		}
+
+		private short[] cubeStrip;
+		public short[] CubeStrip
+		{
+			get { return cubeStrip; }
+			set { cubeStrip = value; }
+		}
+
+		public Cube(Game game)
+			: base(game)
+		{
+		}
+
+		public override void Initialize()
+		{
+			base.Initialize();
+			FillPoints();
+			CreateCubeStrip();
+		}
+
+		protected override void LoadContent()
+		{
+			base.LoadContent();
+		}
+
+		private void FillPoints()
+		{
+			points = new VertexPositionColor[8];
+
+			points[0] = new VertexPositionColor(new Vector3(0, 0, 0), Color.Black);
+			points[1] = new VertexPositionColor(new Vector3(1, 0, 0), Color.Black);
+			points[2] = new VertexPositionColor(new Vector3(0, 1, 0), Color.Black);
+			points[3] = new VertexPositionColor(new Vector3(1, 1, 0), Color.Black);
+			points[4] = new VertexPositionColor(new Vector3(1, 1, 1), Color.Black);
+			points[5] = new VertexPositionColor(new Vector3(0, 1, 1), Color.Black);
+			points[6] = new VertexPositionColor(new Vector3(1, 0, 1), Color.Black);
+			points[7] = new VertexPositionColor(new Vector3(0, 0, 1), Color.Black);
+		}
+
+		private void CreateCubeStrip()
+		{
+			cubeStrip = new short[35];
+
+			cubeStrip[0] = 0;
+			cubeStrip[1] = 2;
+			cubeStrip[2] = 1;
+			cubeStrip[3] = 3;
+
+			cubeStrip[4] = 6;
+
+			cubeStrip[5] = 4;
+			cubeStrip[6] = 5;
+			cubeStrip[7] = 3;
+			cubeStrip[8] = 2;
+			cubeStrip[9] = 0;
+			cubeStrip[10] = 5;
+			cubeStrip[11] = 7;
+			cubeStrip[12] = 6;
+			cubeStrip[13] = 0;
+			cubeStrip[14] = 1;
+		}
+
+		public void Move(String direction)
+		{
+			if (direction == "left")
+			{
+				for (int i = 0; i < points.Length; i++)
+				{
+					points[i].Position.X -= 0.1f;
+				}
+			}
+			else if (direction == "right")
+			{
+				for (int i = 0; i < points.Length; i++)
+				{
+					points[i].Position.X += 0.1f;
+				}
+			}
+			else if (direction == "up")
+			{
+				for (int i = 0; i < points.Length; i++)
+				{
+					points[i].Position.Y += 0.1f;
+				}
+			}
+			else if (direction == "down")
+			{
+				for (int i = 0; i < points.Length; i++)
+				{
+					points[i].Position.Y -= 0.1f;
+				}
+			}
+			else if (direction == "front")
+			{
+				for (int i = 0; i < points.Length; i++)
+				{
+					points[i].Position.Z += 0.1f;
+				}
+			}
+			else if (direction == "back")
+			{
+				for (int i = 0; i < points.Length; i++)
+				{
+					points[i].Position.Z -= 0.1f;
+				}
+			}
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ WindowsGame3/Eindopdracht/Eindopdracht/Game1.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Eindopdracht
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class Game1 : Microsoft.Xna.Framework.Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		private BasicEffect effect;
+		private _3D_axis assenstelsel;
+		public Camera camera1 { get; protected set; }
+		public Camera camera2 { get; protected set; }
+		private Cube cube;
+
+		Viewport defaultViewport;
+		Viewport leftViewport;
+		Viewport rightViewport;
+		Matrix projectionMatrix;
+		Matrix halfprojectionMatrix;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+
+			assenstelsel = new _3D_axis(this);
+			this.Components.Add(assenstelsel);
+
+			projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, 4.0f / 3.0f, 1.0f, 10000f);
+			halfprojectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, 2.0f / 3.0f, 1.0f, 10000f);
+
+			camera1 = new Camera(this, halfprojectionMatrix, new Vector3(0f, 0f, 10f), Vector3.UnitY);
+
+			camera2 = new Camera(this, halfprojectionMatrix, new Vector3(0f, 10f, 0f), Vector3.UnitZ);
+
+			cube = new Cube(this);
+			this.Components.Add(cube);
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			effect = new BasicEffect(GraphicsDevice);
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			defaultViewport = GraphicsDevice.Viewport;
+			leftViewport = defaultViewport;
+			rightViewport = defaultViewport;
+			leftViewport.Width = leftViewport.Width / 2;
+			rightViewport.Width = rightViewport.Width / 2;
+			rightViewport.X = leftViewport.Width;
+			// TODO: use this.Content to load your game content here
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// all content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			// Allows the game to exit
+			if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+				this.Exit();
+
+			if (Keyboard.GetState().IsKeyDown(Keys.Left))
+			{
+				cube.Move("left");
+			}
+			else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+			{
+				cube.Move("right");
+			}
+			else if (Keyboard.GetState().IsKeyDown(Keys.Up) && !Keyboard.GetState().IsKeyDown(Keys.LeftControl))
+			{
+				cube.Move("up");
+			}
+			else if (Keyboard.GetState().IsKeyDown(Keys.Down) && !Keyboard.GetState().IsKeyDown(Keys.LeftControl))
+			{
+				cube.Move("down");
+			}
+			else if (Keyboard.GetState().IsKeyDown(Keys.Up) && Keyboard.GetState().IsKeyDown(Keys.LeftControl))
+			{
+				cube.Move("front");
+			}
+			else if (Keyboard.GetState().IsKeyDown(Keys.Down) && Keyboard.GetState().IsKeyDown(Keys.LeftControl))
+			{
+				cube.Move("back");
+			}
+
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+
+			GraphicsDevice.Viewport = defaultViewport;
+			GraphicsDevice.Clear(Color.White);
+
+			GraphicsDevice.Viewport = leftViewport;
+			effect.World = Matrix.Identity;
+			effect.View = camera1.view;
+			effect.Projection = camera1.projection;
+			effect.VertexColorEnabled = true;
+			foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+			{
+				pass.Apply();
+
+				GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.LineList, assenstelsel.Vertices, 0, 3);
+
+				GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleStrip, cube.Points, 0, 8, cube.CubeStrip, 0, 15);
+			}
+
+			GraphicsDevice.Viewport = rightViewport;
+			effect.World = Matrix.Identity;
+			effect.View = camera2.view;
+			effect.Projection = camera2.projection;
+			effect.VertexColorEnabled = true;
+			foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+			{
+				pass.Apply();
+
+				GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.LineList, assenstelsel.Vertices, 0, 3);
+
+				GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleStrip, cube.Points, 0, 8, cube.CubeStrip, 0, 15);
+			}
+
+
+
+
+			base.Draw(gameTime);
+		}
+	}
+}
